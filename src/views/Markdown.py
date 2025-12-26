b616e1765e106774d95e4ad44d32f1e49c735673
ggtrd <103002419+ggtrd@users.noreply.github.com>
2025-12-26 22:24:24 +0100

    cleaning

--- src/views/Markdown.py
+++ src/views/Markdown.py
@@ -23,21 +23,6 @@ class Markdown:
         return markdown
 
 
-    # # JSON input must be type a dict
-    # def json_to_markdown_helmchart(self):
-    #     with open(self.file_json, 'r') as file:
-    #         json_data = json.load(file)
-
-    #     markdown = f"### {self.title}\n"
-    #     markdown +=  "| Chart name | Chart version | App version |\n"
-    #     markdown +=  "|------------|---------------|-------------|\n"
-
-    #     for chart_name, chart_version, app_version in json_data.items():
-    #         markdown += f"|{chart_name}|{chart_version}||{app_version}|\n"
-   
-    #     return markdown
-
-
     # JSON input must be type a dict
     def json_to_markdown_helmchart(self):
         with open(self.file_json, 'r') as file:
